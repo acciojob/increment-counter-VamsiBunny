63461e5a4766c599b77f457961140ec9687cb1c4
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-09 12:51:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+let text = document.getElementById("counter");
+let body = document.body;
+let sum = 0;
+ 
+function incre() {
+    text.textContent = sum + 1;
+    sum += 1;
+ 
+    let ele = document.createElement("div");
+    ele.setAttribute("class", "newDiv");
+    ele.innerHTML = "<p>localhost:3000 says</p>";
+    ele.innerHTML += sum;
+ 
+    let button = document.createElement("button");
+    button.innerText = "OK";
+    ele.appendChild(button);
+ 
+    ele.style.cssText = "color: white; background-color: black; height: 20vh; padding: 10px; margin-top: 10px;";
+    body.appendChild(ele);
+ 
+    button.addEventListener("click", function() {
+        ele.style.display = "none";
+    });
+}
